25304688ce247eddcdbcdb1ea36d9910784a5dae
Rémy Perona <remperona@gmail.com>
2020-06-01 12:20:44 -0400

    bootstrap the integration tests

--- Tests/Integration/bootstrap.php
+++ Tests/Integration/bootstrap.php
@@ -2,6 +2,10 @@
 
 namespace WP_Rocket\Tests\Integration;
 
+use League\Container\Container;
+use WP_Rocket\Admin\Options;
+use WP_Rocket\Admin\Options_Data;
+use WP_Rocket\Event_Management\Event_Manager;
 use WPMedia\PHPUnit\BootstrapManager;
 use function Patchwork\redefine;
 
@@ -19,6 +23,10 @@ define( 'WP_ROCKET_IS_TESTING', true );
 function load_original_files_before_mocking() {
 	$fixtures = [
 		'/functions.php',
+		'/files.php',
+		'/i18n.php',
+		'/Abstract_Render.php',
+		'/Beacon.php',
 	];
 	foreach ( $fixtures as $file ) {
 		require_once WP_ROCKET_TESTS_FIXTURES_DIR . $file;
@@ -34,5 +42,36 @@ tests_add_filter(
 		define( 'WP_ROCKET_CACHE_ROOT_URL', 'http://example.org/wp-content/cache/' );
 
 		load_original_files_before_mocking();
+
+		$container     = new Container();
+		$event_manager = new Event_Manager();
+
+		$container->add(
+			'options_api',
+			function() {
+				return new Options( 'wp_rocket_' );
+			}
+		);
+
+		$container->add( 'options', 'WP_Rocket\Admin\Options_Data' )
+			->withArgument( $container->get( 'options_api' )->get( 'settings', [] ) );
+
+		$container->add( 'template_path', WP_ROCKET_PLUGIN_ROOT . 'views' );
+		$container->add( 'beacon', 'WP_Rocket\Engine\Admin\Beacon\Beacon' )
+			->withArgument( $container->get( 'options' ) )
+			->withArgument( $container->get( 'template_path' ) . '/settings' );
+
+		$container->addServiceProvider( 'WP_Rocket\Engine\CDN\RocketCDN\ServiceProvider' );
+
+		$subscribers = [
+			'rocketcdn_data_manager_subscriber',
+			'rocketcdn_rest_subscriber',
+			'rocketcdn_admin_subscriber',
+			'rocketcdn_notices_subscriber',
+		];
+
+		foreach ( $subscribers as $subscriber ) {
+			$event_manager->add_subscriber( $container->get( $subscriber ) );
+		}
 	}
 );
